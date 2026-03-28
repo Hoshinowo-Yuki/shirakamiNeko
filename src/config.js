e0574ee13a2470c0fa2517a10b02e5b8f0905f84
星野有希 <hoshinoyuki@lolicon.io>
2026-03-29 03:57:12 +0800

    chore: Update URL

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ export const personalProfileVars = {
   GENERAL_EMAIL: "hi@lolicon.io",
   DEV_EMAIL: "hoshinoyuki@lolicon.io",
   DISCORD_LINK: "https://discordapp.com/users/885756325798227988",
-  DISCORD_IMAGE_LINK: "https://api.lolicon.wtf/discord-api/user/885756325798227988",
+  DISCORD_IMAGE_LINK: "https://api.lolicon.io/discord-api/user/885756325798227988",
   GRAVATAR_LINK: "https://gravatar.com/shirakamineko",
   GOOGLE_GEMINI_CERTIFICATE_LINK: "https://edu.google.accredible.com/11a9f41b-cc6e-4ff9-af82-3c202fdb1ecd#acc.iXJpgVLn",
   YEAR: new Date().getFullYear(),
