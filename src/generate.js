ba85a6014ec429082b40721fc41ce43c83011c6c
Hoshinowo-Yuki <shirakamineko@lolicon.io>
2026-03-29 02:18:50 +0800

    refactor: resolves #1

--- src/generate.js
+++ src/generate.js
@@ -1,12 +1,15 @@
 import { readFileSync, writeFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
 import { personalProfileVars } from "./config.js";
 
-const template = readFileSync("README.template.md", "utf-8");
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const template = readFileSync(join(__dirname, "README.auto.template.md"), "utf-8");
 
 let readme = template;
 for (const [key, value] of Object.entries(personalProfileVars)) {
   readme = readme.replaceAll(`%${key}%`, value);
 }
 
-writeFileSync("README.md", readme);
+writeFileSync(join(__dirname, "..", "README.md"), readme);
 console.log("SUCCESS: README.md updated.");
